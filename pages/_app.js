23c00bcea2e036f2318a4040f09926150a30b785
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-18 16:58:03 +0430

    changed the meta tag injection from _document to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Head>
+        <meta name='viewport' content="width=device-width, minimum-scale=1"></meta>
+      </Head>
+      <Layout>
         <Component {...pageProps} />
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
