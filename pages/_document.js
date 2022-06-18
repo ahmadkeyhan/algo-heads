e86e7d871bfd44b258c8b8b4283c024428c8c442
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-18 16:33:46 +0430

    added viewport meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta name='viewport' content="width=device-width, initial-scale=1"></meta>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
 <link href="https://fonts.googleapis.com/css2?family=Gluten:wght@200;300;600&display=swap" rel="stylesheet"/>
