9e2b9cd2f6383cd0b7c4ad9da49ec66eb065354c
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-18 17:09:03 +0430

    added ESLint | turned walletaddress page to component

--- pages/sholder/[walletAddress].js
+++ pages/sholder/[walletAddress].js
@@ -1,40 +1,9 @@
-
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
+import WalletAddress from "../../components/WalletAddress"
 
 function walletAddress() {
-  const router = useRouter()
-  const { walletAddress } = router.query
-  
-  const [sholderWallet, setSholderWallet] = useState([])
-  const[sales, setSales] = useState()
-  const [isLoading, setLoading] = useState()
-  useEffect(() => {
-      setLoading(true)
-      fetch('../api/nftx')
-        .then((res) => res.json())
-        .then((data) => {
-          // data.message.sales.reverse()
-          // console.log(data)
-          setSales(data.message.sales)
-          setLoading(false)
-        })
-  }, [])
-  // console.log(walletAddress, sales)
-
-  if(sales && walletAddress) {
-    for (var i = 0; i<sales.length; i++) {
-      console.log(sales[i].reciever)
-      if(sales[i].reciever === walletAddress) {
-        console.log(sales[i].nftxUrl.slice(30)*1) 
-      }
-    }
-   //console.log(sholderWallet, walletAddress)
-    return (
-      <p></p>
-    )
-  } 
-
+  return (
+    <WalletAddress />
+  )
 }
 
 export default walletAddress
