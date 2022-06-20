48d66e624a13700e03129499bd8f03030b8986a1
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-20 13:01:26 +0430

    added the getHeads api call to db and a handler to handle post and get

--- pages/api/headlist.js
+++ pages/api/headlist.js
@@ -16,4 +16,33 @@ async function listHeads(req, res) {
     }
 }
 
-export default listHeads
+async function getHeads(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+
+        let heads = await db
+            .collection('heads')
+            .find({})
+            .sort({ _id: -1 })
+            .toArray()
+        return res.json({
+            message: JSON.parse(JSON.stringify(heads))
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'GET': {
+            return getHeads(req, res);
+        }
+
+        case 'POST': {
+            return listHeads(req, res);
+        }
+    }
+}
