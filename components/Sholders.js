431eb62f7b86f3b04aeddf02bac60d8f9c666b3d
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-22 16:12:28 +0430

    sholders index.js created, first functionality (sholder ranking) added

---
+++ components/Sholders.js
@@ -0,0 +1,212 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import narrowStyles from '../styles/sholders.module.css'
+import wideStyles from '../styles/sholdersWide.module.css'
+import { useState, useEffect } from 'react'
+import * as MdIcons from 'react-icons/md'
+import * as SiIcon from 'react-icons/si'
+import * as BsIcons from 'react-icons/bs'
+import { motion, useAnimation } from "framer-motion"
+import NavSlider from './NavSlider'
+import { useRouter } from 'next/router'
+import { lightColorPalette, darkColorPalette } from '../components/colorPalette'
+import { arrowPalette, linkArrowPalette, scrollArrowPalette } from './Assets'
+import Link from 'next/link'
+
+function Sholders() {
+
+  const [activeTheme, setActiveTheme] = useState(document.body.dataset.theme)
+  const inactiveTheme = activeTheme === "light" ? "dark" : "light"
+
+  useEffect(() => {
+    document.body.dataset.theme = activeTheme
+    window.localStorage.setItem("theme", activeTheme)
+  }, [activeTheme])
+
+  const [step, setStep] = useState(0)
+
+  const [heads, setHeads] = useState()
+  const [isLoading, setLoading] = useState()
+  const [headlist, setHeadlist] = useState([])
+  const [addressBook, setAddressBook]=useState([])
+  const [nameBook, setNameBook] = useState([])
+  const [sholderRank, setSholderRank] = useState([])
+  useEffect(() => {
+    fetch('api/headlist')
+      .then((res) => res.json())
+      .then((data) => {
+        setHeads(data.message)
+        data.message.map((head) => {
+          if (addressBook.indexOf(head.sholder.address) === -1) {
+            addressBook.push(head.sholder.address)
+            nameBook.push(head.sholder.name)
+          }
+          headlist.push(head)
+        })
+        addressBook.map((address, index) => {
+            let heads = []
+            headlist.map((head) => {
+                if (head.sholder.address === address) {
+                    heads.push({src: head.src, bgColorCode: head.bgColorCode})
+                }
+            })
+            sholderRank.push({sholder: {address: address, name: nameBook[index]}, heads: heads})
+        })
+        sholderRank.sort((a,b) => b.heads.length - a.heads.length)
+        console.log(sholderRank)
+      })
+  },[])
+
+  const [colorCode, setColorCode] = useState(2)
+
+  const [width, setWidth] = useState()
+  const [height, setHeight] = useState()
+  const [normalizedwidth, setNormalizedWidth] = useState()
+  const [styles, setStyles] = useState(narrowStyles)
+   
+  useEffect(() => {
+    setWidth(window.visualViewport.width)
+    setHeight(window.visualViewport.height)
+
+    if (window.visualViewport.height/window.visualViewport.width >= 16/9) {
+      setNormalizedWidth(100)
+
+    } else {
+      setNormalizedWidth((window.visualViewport.height*900)/(16*window.visualViewport.width))
+      setStyles(wideStyles)
+    } 
+  },[])
+
+  function SholderHolder({top, left, rank}) {
+    return (
+        <motion.div className={styles.sholderCard}
+          style={{color: activeTheme==='light' ? darkColorPalette[sholderRank[rank].heads[0].bgColorCode] : null,
+          backgroundColor: lightColorPalette[sholderRank[rank].heads[0].bgColorCode],
+          top: window.visualViewport.height/window.visualViewport.width >= 16/9 ? `${top}vw` : `${top*9/16}vh`,
+          left: window.visualViewport.height/window.visualViewport.width >= 16/9 ? `${left}vw` : `${left*9/16}vh`}}>
+
+          {/* <motion.div className={rank == step ? styles.firstRank : styles.rank}
+           style={{borderColor: lightColorPalette[sortedHeads[rank].bgColorCode]}}>
+            <p>{rank + 1}</p>
+          </motion.div> */}
+
+          <motion.div className={styles.sholderHolder}>
+            <Image src={sholderRank[rank].heads[0].src} layout='fill' />
+          </motion.div>
+            <motion.div className={styles.sholderCol}>
+            <p>Sholder:</p>
+            <h1>{sholderRank[rank].sholder.name ? sholderRank[rank].sholder.name : sholderRank[rank].sholder.address.slice(0,9)+'...'}</h1>
+            </motion.div>
+            <motion.div className={styles.sholderCol}>
+            <p>carrying:</p>
+            <h1>{sholderRank[rank].heads.length} <span>heads</span></h1>
+            </motion.div>
+        </motion.div>
+      )
+  }
+
+  if(heads) {
+    return (
+        <div className={styles.landing}
+          style={{height: `${normalizedwidth*16/9}vw`,
+          width: `${normalizedwidth}vw`}}>
+          <motion.div className={styles.navSliderFrame}>
+            <NavSlider colorCode={colorCode} />
+          </motion.div>
+          <div className={styles.wheelHolder}>
+            <div className={styles.wheelOne}>
+              <motion.div className={styles.arrowHolder}>
+                <Image className={styles.counterArrows} src={activeTheme === 'light' ? arrowPalette[colorCode] : arrowPalette[7]} layout='fill' />
+              </motion.div>
+              <div className={styles.logoHolder}>
+                <Image className={styles.logo} src={activeTheme==='light'? '/logo.png' : '/darkLogo.png'} layout='fill' />
+              </div>
+            </div>
+            <div className={styles.wheelThree}>
+              <motion.div className={styles.arrowHolder}>
+                <Image className={styles.arrows} src={activeTheme === 'light' ? arrowPalette[colorCode] : arrowPalette[7]} layout='fill' />
+                <motion.div className={styles.stepsForward}>
+                    <motion.div
+                        style={activeTheme==='light'?
+                        {backgroundColor: lightColorPalette[colorCode], color: darkColorPalette[colorCode]} :
+                        {color: lightColorPalette[colorCode], border: `2px solid ${darkColorPalette[colorCode]}`}}
+                        animate={step+4 < sholderRank.length ? null : {display: 'none'}}
+                        onClick={() => setStep(step+1)}
+                        className={styles.step}>
+                        <p>+<span>1</span></p>
+                    </motion.div>
+                    <motion.div
+                        style={activeTheme==='light'?
+                        {backgroundColor: lightColorPalette[colorCode], color: darkColorPalette[colorCode]} :
+                        {color: lightColorPalette[colorCode], border: `2px solid ${darkColorPalette[colorCode]}`}}
+                        animate={step+7 < sholderRank.length ? null : {display: 'none'}}
+                        onClick={() => setStep(step+4)}
+                        className={styles.step}>
+                        <p>+<span>4</span></p>
+                    </motion.div>
+                </motion.div>
+              </motion.div>
+              <motion.div className={styles.counterArrowHolder}>
+                <Image className={styles.counterArrows} src={activeTheme === 'light' ? arrowPalette[colorCode] : arrowPalette[7]} layout='fill' />
+                <motion.div className={styles.stepsBackward}>
+                    <motion.div
+                        style={activeTheme==='light'?
+                        {backgroundColor: lightColorPalette[colorCode], color: darkColorPalette[colorCode]} :
+                        {color: lightColorPalette[colorCode], border: `2px solid ${darkColorPalette[colorCode]}`}}
+                        animate={step > 0 ? null : {display: 'none'}}
+                        onClick={() => setStep(step-1)}
+                        className={styles.step}>
+                        <p>-<span>1</span></p>
+                    </motion.div>
+                    <motion.div
+                        style={activeTheme==='light'?
+                        {backgroundColor: lightColorPalette[colorCode], color: darkColorPalette[colorCode]} :
+                        {color: lightColorPalette[colorCode], border: `2px solid ${darkColorPalette[colorCode]}`}}
+                        animate={step > 3 ? null : {display: 'none'}}
+                        onClick={() => setStep(step-4)}
+                        className={styles.step}>
+                        <p>-<span>4</span></p>
+                    </motion.div>
+                </motion.div>
+              </motion.div>
+                <SholderHolder top={-11} left={46} rank={step} />
+                <SholderHolder top={16} left={70} rank={1+step} />
+                <SholderHolder top={43} left={70} rank={2+step} />
+                <SholderHolder top={70} left={46} rank={3+step} />
+            </div>
+            <div className={styles.wheelFour}>
+            </div>
+          </div>
+        </div>
+        )
+  } else {
+    return (
+        <div className={styles.landing}
+          style={{height: `${normalizedwidth*16/9}vw`,
+          width: `${normalizedwidth}vw`}}>
+          <motion.div className={styles.navSliderFrame}>
+            <NavSlider colorCode={colorCode} />
+          </motion.div>
+          <div className={styles.wheelHolder}>
+            <div className={styles.wheelOne}>
+              <motion.div className={styles.arrowHolder}>
+                <Image className={styles.counterArrows} src={activeTheme === 'light' ? arrowPalette[colorCode] : arrowPalette[7]} layout='fill' />
+              </motion.div>
+              <div className={styles.logoHolder}>
+                <Image className={styles.logo} src={activeTheme==='light'? '/logo.png' : '/darkLogo.png'} layout='fill' />
+              </div>
+            </div>
+            <div className={styles.wheelThree}>
+              <motion.div className={styles.arrowHolder}>
+                <Image className={styles.counterArrows} src={activeTheme === 'light' ? arrowPalette[colorCode] : arrowPalette[7]} layout='fill' />
+              </motion.div>
+            </div>
+            <div className={styles.wheelFour}>
+            </div>
+          </div>
+        </div>
+        )
+  }
+ }
+ 
+ export default Sholders
