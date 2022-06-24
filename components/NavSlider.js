918cbefb73905cf9ee77040701fdc919f00a12a1
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-24 17:59:18 +0430

    navslider now catchers sholders/[sholder]

--- components/NavSlider.js
+++ components/NavSlider.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import * as MdIcons from 'react-icons/md'
 import * as RiIcons from 'react-icons/ri'
+import * as CgIcons from 'react-icons/cg'
 import { motion, motionValue, useViewportScroll } from "framer-motion"
 import { darkColorPalette, lightColorPalette } from './colorPalette'
 
@@ -17,7 +18,7 @@ export default function NavSlider({colorCode}) {
   })
 
   const router = useRouter()
-  const routes = ['/','/headlist','/roadmap']
+  const routes = ['/','/headlist','/sholders','/roadmap']
 
   function NavDot({route}) {
     return(
@@ -35,17 +36,20 @@ export default function NavSlider({colorCode}) {
           style={{borderColor: darkColorPalette[colorCode]}}
           className={styles.navSlider}>
           <motion.div style={{color: darkColorPalette[colorCode]}} className={styles.routeIcon}>
-            {routes.indexOf(router.route) == 0 ? <MdIcons.MdHome />:
-            routes.indexOf(router.route) == 1 ? <MdIcons.MdFace />:<RiIcons.RiRouteFill />}
+            {routes.indexOf(router.route.slice(0,9)) == 0 ? <MdIcons.MdHome />:
+            routes.indexOf(router.route.slice(0,9)) == 1 ? <MdIcons.MdFace />:
+            routes.indexOf(router.route.slice(0,9)) == 2 ? <CgIcons.CgTrophy />:
+            <RiIcons.RiRouteFill />}
           </motion.div>
           <motion.div 
             style={{color: darkColorPalette[colorCode],borderColor: darkColorPalette[colorCode],backgroundColor: lightColorPalette[colorCode]}}
-            animate={{top: `${routes.indexOf(router.route)*2.25-0.1875}rem`}}
+            animate={{top: `${routes.indexOf(router.route.slice(0,9))*1.5-0.1875}rem`}}
             transition={{duration: 0.2}}
             className={styles.routeCatcher} />
           <NavDot route={routes[0]} />
           <NavDot route={routes[1]} />
           <NavDot route={routes[2]} />
+          <NavDot route={routes[3]} />
         </div>
     </div>
   )
