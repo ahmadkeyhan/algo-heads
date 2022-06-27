4685589a2a85e9f7c4294c63fa455b11f23c8c74
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-06-27 13:00:22 +0430

    added a separate asset component for proud heads

---
+++ components/ProudHeads.js
@@ -0,0 +1,8 @@
+export const proudHeads = ['/algoHead046.png',
+'/algoHead047.png',
+'/algoHead048.png',
+'/algoHead049.png',
+'/algoHead050.png',
+'/HappyPride!.png',
+'/HappyPride!.png',
+'/HappyPride!.png']
