ce13367a1d833d02366caddddeac1257ad9404d6
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-07-02 15:43:46 +0430

    added the sholder api to get and post sholders

---
+++ pages/api/sholders.js
@@ -0,0 +1,48 @@
+const { connectToDatabase } = require('../../lib/mongodb')
+
+async function listSholders(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+        let sholder = req.body;
+        sholder = JSON.parse(sholder);
+        await db.collection('sholders').updateOne({address: sholder.address}, {$set:sholder}, {upsert: true})
+        return res.json({
+            message: 'sholder listed!'
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+async function getSholders(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+
+        let sholders = await db
+            .collection('sholders')
+            .find({})
+            .sort({ _id: -1 })
+            .toArray()
+        return res.json({
+            message: JSON.parse(JSON.stringify(sholders))
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'GET': {
+            return getHeads(req, res);
+        }
+
+        case 'POST': {
+            return listHeads(req, res);
+        }
+    }
+}
