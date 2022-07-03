792cf151a076ed520fcd2e2f6e43b0ce0c2bcf02
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-07-03 22:37:59 +0430

    I fixed the metadata for ah001-ah015

---
+++ components/metaData.js
@@ -0,0 +1,221 @@
+export const metaData = [
+    {
+        // ['blue', 'green', 'yellow', 'orange', 'red', 'purple',
+        // 'grey', 'proudPink', 'paleBlue', 'paleOrange', 'palePurple']
+        backgroundColor: '',
+        
+        // ['blue', 'green', 'yellow', 'orange', 'red', 'purple',
+        // 'grey', 'proudWhite', 'paleBlue', 'paleOrange', 'palePurple']
+        frameColor: '',
+
+                // ['blue', 'green', 'yellow', 'orange', 'red', 'purple',
+        // 'grey', 'proudPink', 'paleBlue', 'paleOrange', 'palePurple']
+        skinColor: '',
+
+                // ['blue', 'green', 'yellow', 'orange', 'red', 'purple',
+        // 'grey', 'proudPink', 'paleBlue', 'paleOrange', 'palePurple']
+        hairColor: '',
+
+        // ['codeHead', 'cartoonHead', 'radioHead', 'slivioHead', 'abbyHead',
+        // 'coconutHead', 'mohawkHead', 'creamHead', 'masculineSkull', 'feminineSkull',
+        // 'pentagonalHead', 'abrahamHead', 'maskulineHead', 'femaskHead', 'femmeFatale',
+        // 'umbrellaHead', 'puffHead', 'ponyHead', 'logoHead', 'scheherazadeHead',
+        // 'catHatter1', 'catHatter2', 'catHatter3']
+        head:'',
+
+        //['10/10', '20/20', '10/20', '0/20', '30/30',
+        //'']
+        eyesight: '',
+
+        // ['open', 'close']
+        mouth: '',
+
+        //['earing', 'mask', 'crown', 'piercing', 'glasses',
+        // 'none']
+        accessory1: '',
+
+        //['earing', 'mask', 'crown', 'piercing', 'glasses',
+        // 'none']
+        accessory2: ''
+    },
+    {
+        assetName: 'ah001',
+        backgroundColor: 'orange',
+        frameColor: 'grey',
+        skinColor: 'green',
+        hairColor: 'green',
+        head:'abby',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'earing',
+        accessory2: 'crown'
+    },
+    {
+        assetName: 'ah002',
+        backgroundColor: 'blue',
+        frameColor: 'yellow',
+        skinColor: 'yellow',
+        hairColor: 'yellow',
+        head:'codeHead',
+        eyesight: '20/20',
+        mouth: 'open',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah003',
+        backgroundColor: 'red',
+        frameColor: 'yellow',
+        skinColor: 'blue',
+        hairColor: 'blue',
+        head:'scheherazade',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah004',
+        backgroundColor: 'purple',
+        frameColor: 'orange',
+        skinColor: 'orange',
+        hairColor: 'orange',
+        head:'coconut',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah005',
+        backgroundColor: 'blue',
+        frameColor: 'orange',
+        skinColor: 'blue',
+        hairColor: 'sunKiss',
+        head:'feminineSkull',
+        eyesight: '20/20',
+        mouth: 'open',
+        accessory1: 'piercing',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah006',
+        backgroundColor: 'green',
+        frameColor: 'purple',
+        skinColor: 'red',
+        hairColor: 'red',
+        head:'cartoonHead',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah007',
+        backgroundColor: 'green',
+        frameColor: 'blue',
+        skinColor: 'grey',
+        hairColor: 'grey',
+        head:'radioHead',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'piercing',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah008',
+        backgroundColor: 'purple',
+        frameColor: 'grey',
+        skinColor: 'green',
+        hairColor: 'green',
+        head:'silvio',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah009',
+        backgroundColor: 'blue',
+        frameColor: 'green',
+        skinColor: 'red',
+        hairColor: 'red',
+        head:'puffy',
+        eyesight: '30/30',
+        mouth: 'closed',
+        accessory1: 'earing',
+        accessory2: 'crown'
+    },
+    {
+        assetName: 'ah010',
+        backgroundColor: 'red',
+        frameColor: 'grey',
+        skinColor: 'yellow',
+        hairColor: 'transparent',
+        head:'masculineSkull',
+        eyesight: '17/20',
+        mouth: 'closed',
+        accessory1: 'glasses',
+        accessory2: 'wire'
+    },
+    {
+        assetName: 'ah011',
+        backgroundColor: 'green',
+        frameColor: 'purple',
+        skinColor: 'orange',
+        hairColor: 'orange',
+        head:'scheherazade',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'earing',
+        accessory2: 'mask'
+    },
+    {
+        assetName: 'ah012',
+        backgroundColor: '',
+        frameColor: 'red',
+        skinColor: 'blue',
+        hairColor: 'blue',
+        head:'mohawk',
+        eyesight: '10/10',
+        mouth: 'open',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah013',
+        backgroundColor: '',
+        frameColor: 'grey',
+        skinColor: 'red',
+        hairColor: 'red',
+        head:'abby',
+        eyesight: '20/20',
+        mouth: 'open',
+        accessory1: 'none',
+        accessory2: 'none'
+    },
+    {
+        assetName: 'ah014',
+        backgroundColor: 'grey',
+        frameColor: 'red',
+        skinColor: 'purple',
+        hairColor: 'purple',
+        head:'coconut',
+        eyesight: '20/20',
+        mouth: 'closed',
+        accessory1: 'earing',
+        accessory2: 'mask'
+    },
+    {
+        assetName: 'ah015',
+        backgroundColor: 'yellow',
+        frameColor: 'orange',
+        skinColor: 'orange',
+        hairColor: 'blue',
+        head:'catHatter',
+        eyesight: '40/40',
+        mouth: 'closed',
+        accessory1: 'none',
+        accessory2: 'crown'
+    },
+]
