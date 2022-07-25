8bd8a8f3af86dc902a4056478f2348e9c39867d1
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-07-25 23:04:27 +0430

    created the api to get auctions and update them

---
+++ pages/api/auctions.js
@@ -0,0 +1,48 @@
+const { connectToDatabase } = require('../../lib/mongodb')
+
+async function updateAuction(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+        let auction = req.body;
+        auction = JSON.parse(auction);
+        await db.collection('auctions').updateOne({_id: auction._id}, {$set:auction}, {upsert: true})
+        return res.json({
+            message: 'Auction updated!'
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+async function getAuctions(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+
+        let auctions = await db
+            .collection('auctions')
+            .find({})
+            .sort({ _id: -1 })
+            .toArray()
+        return res.json({
+            message: JSON.parse(JSON.stringify(auctions))
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'GET': {
+            return getAuctions(req, res);
+        }
+
+        case 'POST': {
+            return updateAuction(req, res);
+        }
+    }
+}
