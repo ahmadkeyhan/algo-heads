bc8151acce4711c0ac19859b8d8c5c62c66961d6
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-07-31 00:54:31 +0430

    update shuffle after registery

--- pages/api/shuffles.js
+++ pages/api/shuffles.js
@@ -1,6 +1,23 @@
 const { connectToDatabase } = require('../../lib/mongodb')
+var ObjectId = require('mongodb').ObjectId
 
-export default async function getTasks(req, res) {
+async function updateShuffles(req, res) {
+    try {
+        let { db } = await connectToDatabase();
+        let shuffle = req.body;
+        shuffle = JSON.parse(shuffle);
+        await db.collection('shuffles').updateOne({"_id": ObjectId(shuffle._id)}, {$set:{registery: shuffle.registery, lifeCycle: shuffle.lifeCycle}}, {upsert: true})
+        return res.json({
+            message: 'Shuffle updated!'
+        })
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message
+        })
+    }
+}
+
+async function getShuffles(req, res) {
     try {
         let { db } = await connectToDatabase();
         let shuffles = await db
@@ -15,5 +32,17 @@ export default async function getTasks(req, res) {
         return res.json({
             message: new Error(error).message
         })
+    }
+}
+
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'GET': {
+            return getShuffles(req, res);
+        }
+
+        case 'POST': {
+            return updateShuffles(req, res);
+        }
     }
 }
