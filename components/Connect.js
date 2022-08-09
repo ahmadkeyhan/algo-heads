a9943d005235e89d53b6cc0f0efbcc5e3abc0c01
ahmadkeyhan <keyhan.ahmad@gmail.com>
2022-08-09 15:30:40 +0430

    deleted the connection component

--- components/Connect.js
+++
@@ -1,8 +0,0 @@
-import MyAlgoConnect from '@randlabs/myalgo-connect'
-
-export const myAlgoConnect = new MyAlgoConnect({ disableLedgerNano: false })
-export const settings = {
-        shouldSelectOneAccount: true,
-        openManager: true
-      }
-
